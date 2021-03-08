d4212772aa7dbd1f974f122b287f6a6a7a83c4fb
FethyAmr <74984977+FethyAmr@users.noreply.github.com>
2021-03-08 16:33:26 +0100

    My First Commit Js

---
+++ jour-1/jour01.js
@@ -0,0 +1,40 @@
+console.log("Hello World !");
+
+var test;
+test= "My name is Fethy"
+console.log(test)
+
+var name;
+name= "fethy"
+console.log("Nice to meet you "+ name)
+
+var testLength;
+testLength= " I'm very long!"
+console.log(testLength.length)
+
+var food;
+food= "croissant is meh"
+food= food.replace( "meh","so good") 
+console.log(food)
+
+var basic
+basic= "This is good"
+var basicUp= basic.toUpperCase()
+var basicDown= basic.toLowerCase()
+console.log(basic + basicUp + "" + basicDown)
+
+var Word
+Word="banana"
+var letters = Word.split("");
+console.log(letters);
+
+var age
+age = 23
+var template
+template= "I'm 23 years old"
+console.log(`I'm ${age} years old`);
+
+var changed
+changed= "bonjour"
+changed= changed.replace(/o/g,"a")
+console.log(changed)
