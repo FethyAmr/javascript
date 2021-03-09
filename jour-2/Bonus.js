82d8c2faceda6c40492600c4d70be438b2243974
FethyAmr <74984977+FethyAmr@users.noreply.github.com>
2021-03-09 17:31:41 +0100

    Jour 2 plus Bonus

---
+++ jour-2/Bonus.js
@@ -0,0 +1,12 @@
+var random = Math.random();
+var min = 1;
+var max = 6;
+Math.floor(random * (max - min + 1)+ min);
+if (random === max){
+    console.log("Yes I win !")
+}
+else {
+    console.log("So close !")
+}
+
+
---
+++ jour-2/Bonus2.js
@@ -0,0 +1,17 @@
+var month = "January";
+switch (month) {
+    case "Winter": 
+        console.log("Let it snow !");
+        break;
+    case "Spring": 
+        console.log("Flowers everywhere !");
+        break;
+    case "Summer": 
+        console.log("Let's go to the beach");
+        break;
+    case "Fall": 
+        console.log("Let it snow !");
+        break;
+    default: 
+        console.log("That's not a season...");
+}
