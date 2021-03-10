aea3b60c96f88f852d9145d88bfeff5bfa1c9585
FethyAmr <74984977+FethyAmr@users.noreply.github.com>
2021-03-10 17:24:19 +0100

    Javascript Jour3 Plus Bonus

---
+++ jour-3/Bonus.js
@@ -0,0 +1,25 @@
+for (var i = 0; i<= 100; i++)
+    if (i%3) {
+        console.log("fizz")
+        break
+    }
+
+    else if (i%5){
+        console.log("buzz");
+        break
+    }
+
+    else if (i % 3 && i % 5){
+        console.log("fizzbuzz");
+        break
+    }
+
+    else if (i % 7){
+     console.log();   
+     break
+    }
+
+    else{
+        console.log(i);
+        break
+    }
---
+++ jour-3/Bonus2.js
@@ -0,0 +1,10 @@
+total = 0
+i = 1
+limit = 55
+
+while(i < limit){
+total=total+i++;
+console.log(total)
+}
+
+
