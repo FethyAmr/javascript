b2c1517c536f5dbfa0060909fdba1c3ea435e3df
FethyAmr <74984977+FethyAmr@users.noreply.github.com>
2021-03-11 16:03:10 +0100

    JavascriptJour4

---
+++ jour-4/jour04.js
@@ -0,0 +1,29 @@
+var cat ={
+
+name: "Garfield",
+age: 3,
+isCute: true,
+
+}
+
+console.log(cat);
+console.log(cat["age"]);
+console.log(cat["isCute"]);
+
+
+var cat2 ={
+
+name: "Naif",
+age: 20,
+isCute: true,
+
+}
+Object.values(cat,cat2);
+console.log(cat, cat2);
+console.log(cat["age"]);
+console.log(cat2["isCute"]);
+
+function checkIEven (num){
+
+    if checkIEven  
+}
