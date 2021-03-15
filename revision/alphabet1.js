1adef6c886aac20a0eb7c790943da2ba0b8b1c2e
FethyAmr <74984977+FethyAmr@users.noreply.github.com>
2021-03-15 17:03:51 +0100

    Javascript Exo- Révision

---
+++ revision/alphabet1.js
@@ -0,0 +1,22 @@
+// 01 - Alphabet
+// - Créer une fonction `sortLetters` qui reçoit une string en paramètre
+// - La fonction retourne une string avec les mêmes lettres, dans l'ordre alphabétique
+// - Appelez votre fonction avec "konexio" comme argument et vérifiez que vous obtenez "eiknoox"
+
+
+
+var myWord;
+
+function sortLetters(word) {
+  myWord = word.split('');
+  
+  myWord = myWord.reverse();
+
+  myWord = myWord.join("")
+
+  return myWord;
+}
+
+sortLetters('konexio');
+
+console.log(myWord);
