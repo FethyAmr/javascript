b9199e8c851516dd970e21d1737b8ad07f14ccd9
FethyAmr <74984977+FethyAmr@users.noreply.github.com>
2021-03-16 17:27:29 +0100

    Jour5

---
+++ jour-5/01-Calculator/Calculator.js
@@ -0,0 +1,21 @@
+function calculator(Int1, operator, Int2){
+
+    if(operator === "+"){
+        console.log(Int1 + Int2);
+        
+    }
+    if(operator === "-"){
+        console.log(Int1 - Int2);
+        
+    }
+    if(operator === "x"){
+        console.log(Int1 * Int2);
+        
+    }
+    if(operator === "/"){
+        console.log(Int1 / Int2);
+        
+    }
+
+}
+calculator(4,"x",5)
