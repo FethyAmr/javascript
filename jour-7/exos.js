40cf599158d684eeb5e708ff73607065a6d1fb35
FethyAmr <74984977+FethyAmr@users.noreply.github.com>
2021-03-22 16:18:00 +0100

    exos 1,2,3,4,5 Jour7

--- jour-7/exos.js
+++ jour-7/exos.js
@@ -40,10 +40,20 @@
 
 // Exos 4 Filter Numbers 
 
-var array = [1, "toto", 34, "javascript", 8];
+// var array = [1, "toto", 34, "javascript", 8];
 
- var numbers = array.filter(function (num){
-    return num >=0
- })
+//  var numbers = array.filter(function (num){
+//     return num >=0
+//  })
 
-console.log(numbers)
+// console.log(numbers)
+
+// Exos 5  Filter Even
+
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+
+var even = numbers.filter(function (num){
+return num %2 === 0
+
+})
+console.log(even)
