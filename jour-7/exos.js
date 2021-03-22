adb0f879149a64350a414ccc79320fe96337440f
FethyAmr <74984977+FethyAmr@users.noreply.github.com>
2021-03-22 16:32:27 +0100

    exos 1,2,3,4,5 Jour7

--- jour-7/exos.js
+++ jour-7/exos.js
@@ -40,20 +40,49 @@
 
 // Exos 4 Filter Numbers 
 
-// var array = [1, "toto", 34, "javascript", 8];
-
-//  var numbers = array.filter(function (num){
-//     return num >=0
-//  })
-
-// console.log(numbers)
+var array = [1, "toto", 34, "javascript", 8]
+var numbers = array.filter( function (elem){
+    if (typeof elem === 'number' )
+    {return elem}
+})
+console.log(numbers)
 
 // Exos 5  Filter Even
 
-var numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+// var numbers = [1, 2, 3, 4, 5, 6, 7, 8]
 
-var even = numbers.filter(function (num){
-return num %2 === 0
+// var even = numbers.filter(function (num){
+// return num %2 === 0
 
-})
-console.log(even)
+// })
+// console.log(even)
+
+// Exos 6 Cakes 
+
+var cakes = [
+	{
+		name: "cake",
+		flavor: "vanilla",
+		status: "available"
+	},
+	{
+		name: "brownie",
+		flavor: "chocolate",
+		status: "available"
+	},
+	{
+		name: "pie",
+		flavor: "strawberry",
+		status: "available"
+	},
+	{
+		name: "muffin",
+		flavor: "pistachio",
+		status: "available"
+	},
+	{
+		name: "donut",
+		flavor: "chocolate",
+		status: "available"
+	},
+]
