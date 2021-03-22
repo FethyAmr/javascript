732d5e56335bdb9aeba36e718af7aecfbf8e1299
FethyAmr <74984977+FethyAmr@users.noreply.github.com>
2021-03-22 16:09:47 +0100

    exos 1,2,3,4 Jour7

--- jour-7/exos.js
+++ jour-7/exos.js
@@ -1,39 +1,49 @@
-var fs = require("fs");
+// var fs = require("fs");
 
-fs.readFile("jour-7.txt", function (err, data) {
-    if (err) {
-        console.error(err);
-        return;
-    }
+// fs.readFile("jour-7.txt", function (err, data) {
+//     if (err) {
+//         console.error(err);
+//         return;
+//     }
 
-    console.log("Lecture en différé : " + data.toString());
-});
-
-
-//Exos 2 Map Double 
-
-var array = [1, 2, 3, 4, 5];
-
-var doubles = array.map(function (num) {
-    return num * 2;
-});
-console.log(doubles);
+//     console.log("Lecture en différé : " + data.toString());
+// });
 
 
-// Exos 3 Map Names   
+// //Exos 2 Map Double 
 
-var longNames = [
-    {
-        firstName: "Jane",
-        lastName: "Doe"
-    },
-    {
-        firstName: "John",
-        lastName: "Smith"
-    }
-]
+// var array = [1, 2, 3, 4, 5];
 
-var shortNames = longNames.map(function (elem) {
-    return  elem.firstName + " " + elem.lastName
-});
-console.log(shortNames);
+// var doubles = array.map(function (num) {
+//     return num * 2;
+// });
+// console.log(doubles);
+
+
+// // Exos 3 Map Names   
+
+// var longNames = [
+//     {
+//         firstName: "Jane",
+//         lastName: "Doe"
+//     },
+//     {
+//         firstName: "John",
+//         lastName: "Smith"
+//     }
+// ]
+
+// var shortNames = longNames.map(function (elem) {
+//     return  elem.firstName + " " + elem.lastName
+// });
+// console.log(shortNames);
+
+// Exos 4 Filter Numbers 
+
+var array = [1, "toto", 34, "javascript", 8];
+
+ var numbers = array.filter(function (num){
+    return num >=0
+ })
+
+console.log(numbers)
