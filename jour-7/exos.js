110d1c3bb5cd0f844e7d8f6480475ba3198f45ad
FethyAmr <74984977+FethyAmr@users.noreply.github.com>
2021-03-22 15:37:57 +0100

    exos 1,2,3 Jour7

---
+++ jour-7/exos.js
@@ -0,0 +1,39 @@
+// var fs = require("fs");
+
+// fs.readFile("jour-7.txt", function (err, data) {
+//     if (err) {
+//         console.error(err);
+//         return;
+//     }
+
+//     console.log("Lecture en différé : " + data.toString());
+// });
+
+
+//Exos 2 Map Double 
+
+var array = [1, 2, 3, 4, 5];
+
+var doubles = array.map(function (num) {
+    return num * 2;
+});
+console.log(doubles);
+
+
+// Exos 3 Map Names   
+
+var longNames = [
+    {
+        firstName: "Jane",
+        lastName: "Doe"
+    },
+    {
+        firstName: "John",
+        lastName: "Smith"
+    }
+]
+
+var shortNames = longNames.map(function (elem) {
+    return  elem.firstName +" " + elem.lastName
+});
+console.log(shortNames);
